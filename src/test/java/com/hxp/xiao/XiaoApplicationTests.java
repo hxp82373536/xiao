b0a48e30d164977d384e504efa7e65049d0221dc
hxp <huangxp414@163.com>
2020-08-19 17:56:00 +0800

    java8 stream的简单示例

--- src/test/java/com/hxp/xiao/XiaoApplicationTests.java
+++ src/test/java/com/hxp/xiao/XiaoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hxp.xiao;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XiaoApplicationTests {
 
+	/**
+	 * https://github.com/wslong/jetbrains-agent/tree/master/v3.2.1
+	 * 半径为2m的半球形盛满水水池，若将水从上方全抽出所做功为Q，则Q/2时抽去水之百分比（答案为73.25%）;
+	 * 将73.25%做1亿次md2加密
+	 * 最终结果：b64488729b831330072b051ce45172c8
+	 * 破解插件地址：https://gitee.com/pengzhile/ide-eval-resetter
+	 */
 	@Test
 	void contextLoads() {
+		String val = "73.25%";
+		for (int i = 0; i < 100000000; i++) {
+			val = DigestUtils.md2Hex(val);
+			System.out.println(i);
+		}
+		System.out.println("最终结果：" + val);
 	}
 
 }
