72bba78bd89e06d9a541c1f9c10855f611110dc8
hxp <huangxp414@163.com>
2020-08-19 14:02:10 +0800

    base by spring-boot-2.3.3 with devtools web and lombok

---
+++ src/main/java/com/hxp/xiao/XiaoApplication.java
@@ -0,0 +1,13 @@
+package com.hxp.xiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hxp/xiao/XiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxp.xiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
